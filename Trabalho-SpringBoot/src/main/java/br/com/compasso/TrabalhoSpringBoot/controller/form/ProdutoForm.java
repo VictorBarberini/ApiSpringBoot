a3037f6cc382314f5165559a13e561e119097fdd
VictorBarberini <49589141+VictorBarberini@users.noreply.github.com>
2021-02-22 16:02:59 -0300

    Update ProdutoForm.java

--- Trabalho-SpringBoot/src/main/java/br/com/compasso/TrabalhoSpringBoot/controller/form/ProdutoForm.java
+++ Trabalho-SpringBoot/src/main/java/br/com/compasso/TrabalhoSpringBoot/controller/form/ProdutoForm.java
@@ -1,10 +1,10 @@
 package br.com.compasso.TrabalhoSpringBoot.controller.form;
 
 import java.math.BigDecimal;
-
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 import br.com.compasso.TrabalhoSpringBoot.modelo.Estoque;
 import br.com.compasso.TrabalhoSpringBoot.modelo.Produto;
 import br.com.compasso.TrabalhoSpringBoot.repository.EstoqueRepository;
@@ -13,9 +13,9 @@ public class ProdutoForm {
 
 	@NotNull @NotEmpty
 	private String descricao;
-	@NotNull
+	@NotNull  @DecimalMin("0.00")
 	private BigDecimal valor;
-	@NotNull
+	@NotNull @Min(0)
 	private Integer quantidade;
 	
 	
